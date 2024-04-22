191513dc14467111987603e7445557a2395cd330
tushar909t <tusharahmed941@gmail.com>
2024-04-22 12:27:25 +0600

    old github flagged new repos

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,1 +1,3 @@
-export const baseURL = "http://localhost:8080/api/v1";
+// export const baseURL = "http://localhost:8080/api/v1";
+export const baseURL =
+  "https://inventory-management-backend-j0ss.onrender.com/api/v1";
